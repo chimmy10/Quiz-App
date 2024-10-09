a6ee323b57fae3dd49a23290e819908124f67f5a
Abstract Programmer <AbstractProgrammer@example.com>
2024-10-09 15:40:41 +0100

    first commit

---
+++ src/components/Question.js
@@ -0,0 +1,19 @@
+import { useQuiz } from "../Contexts/QuizContext";
+import Options from "./Options";
+
+function Question() {
+	const { questions, index, dispatch, answer } = useQuiz();
+
+	return (
+		<div>
+			<h4>{questions[index].question}</h4>
+			<Options
+				question={questions[index]}
+				dispatch={dispatch}
+				answer={answer}
+			/>
+		</div>
+	);
+}
+
+export default Question;
